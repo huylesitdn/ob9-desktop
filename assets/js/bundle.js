a90fb9a431f433dec33cfcd29a8d05bfbe714d19
Hp Lâm <hplam@Mac.local>
2022-04-06 23:25:41 +0700

    setup project, universal page, login page, register page

---
+++ assets/js/bundle.js
@@ -0,0 +1,252 @@
+function getUrlVars() {
+  var vars = [],
+    hash;
+  var hashes = window.location.href
+    .slice(window.location.href.indexOf("?") + 1)
+    .split("&");
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split("=");
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
+
+// Translator
+
+const LANGUAGES = {
+  EN: "en",
+  ZH: "zh",
+};
+
+var translator = new Translator({
+  defaultLanguage: "en",
+  detectLanguage: true,
+  selector: "[data-i18n]",
+  debug: false,
+  registerGlobally: "__",
+  persist: true,
+  persistKey: "preferred_language",
+  filesLocation: "/assets/i18n",
+});
+
+const PREFERED_REGION = "preferred_region";
+const _get_translator_config =
+  translator.config.persistKey || "preferred_language";
+const _get_language =
+  localStorage.getItem(_get_translator_config) || LANGUAGES.EN;
+const _get_region = localStorage.getItem(PREFERED_REGION);
+
+translator.fetch([LANGUAGES.EN, LANGUAGES.ZH]).then(() => {
+  // -> Translations are ready...
+  translator.translatePageTo(_get_language);
+  changeLanguageColor();
+});
+
+/**
+ * MENU SLIDE
+ *
+ */
+
+$("#navMenu").on("click", function (e) {
+  $("#mySidenav").addClass("active");
+});
+
+$("#mySidenav .backdrop, #mySidenav a.left-nav__top__nav__item__link").on(
+  "click",
+  function (e) {
+    $("#mySidenav").removeClass("active");
+  }
+);
+
+const selectLanguageModalElm = $("#selectLanguage");
+if (selectLanguageModalElm.length > 0) {
+  var selectLanguageModal = new bootstrap.Modal(selectLanguageModalElm, {});
+}
+$(".choose-language").on("click", function (e) {
+  const select_language = $(this).data("language");
+  const select_region = $(this).data("region");
+  const accept_languages = ["Malaysia", "Singapore"];
+
+  if (!accept_languages.includes(select_region)) {
+    window.location.href = "/access-denied.html";
+    return false;
+  }
+
+  if (LANGUAGES[select_language]) {
+    translator.translatePageTo(LANGUAGES[select_language]);
+    selectLanguageModal.hide();
+    $("#mySidenav").removeClass("active");
+    localStorage.setItem(PREFERED_REGION, select_region);
+    changeLanguageColor();
+    window.location.reload();
+  } else {
+    console.log("No language setup");
+  }
+});
+
+$(".universal__content__language").on("click", function (e) {
+  const select_language = $(this).data("language");
+  if (LANGUAGES[select_language]) {
+    translator.translatePageTo(LANGUAGES[select_language]);
+    window.location.href = "/";
+  } else {
+    console.log("No language setup");
+  }
+});
+
+$(".universal .play-now a").on("click", function (e) {
+  e.preventDefault();
+  const slick_current_select = $(
+    "#selectLanguage .slick-list .slick-track .slick-current .title"
+  );
+  if (slick_current_select.length > 0) {
+    const slick_current_select_title = slick_current_select.data("i18n");
+    const accept_languages = [
+      "universal_page.Malaysia",
+      "universal_page.Singapore",
+    ];
+    if (accept_languages.includes(slick_current_select_title)) {
+      window.location.href = "/login.html";
+    } else {
+      window.location.href = "/access-denied.html";
+    }
+  }
+});
+
+function changeLanguageColor() {
+  $(".choose-language").each(function () {
+    const get_attr_lang = $(this).data("language").toLowerCase();
+    const get_attr_region = $(this).data("region");
+    const _get_region = localStorage.getItem(PREFERED_REGION);
+    if (_get_language == get_attr_lang && _get_region == get_attr_region) {
+      $(this).addClass("text-primary");
+    }
+  });
+}
+
+/**
+ * MENU SLIDE
+ *
+ */
+
+/**
+ * SCROLL TEXT
+ *
+ */
+
+//this is the useful function to scroll a text inside an element...
+function startScrolling(scroller_obj, velocity, start_from) {
+  //bind animation  inside the scroller element
+  scroller_obj
+    .bind("marquee", function (event, c) {
+      //text to scroll
+      var ob = $(this);
+      //scroller width
+      var sw = parseInt(ob.closest(".text-animated").width());
+      //text width
+      var tw = parseInt(ob.width());
+      //text left position relative to the offset parent
+      var tl = parseInt(ob.position().left);
+      //velocity converted to calculate duration
+      var v = velocity > 0 && velocity < 100 ? (100 - velocity) * 1000 : 5000;
+      //same velocity for different text's length in relation with duration
+      var dr = (v * tw) / sw + v;
+      //is it scrolling from right or left?
+      switch (start_from) {
+        case "right":
+          //   console.log('here')
+          //is it the first time?
+          if (typeof c == "undefined") {
+            //if yes, start from the absolute right
+            ob.css({
+              left: sw,
+            });
+            sw = -tw;
+          } else {
+            //else calculate destination position
+            sw = tl - (tw + sw);
+          }
+          break;
+        default:
+          if (typeof c == "undefined") {
+            //start from the absolute left
+            ob.css({
+              left: -tw,
+            });
+          } else {
+            //else calculate destination position
+            sw += tl + tw;
+          }
+      }
+      //attach animation to scroller element and start it by a trigger
+      ob.animate(
+        {
+          left: sw,
+        },
+        {
+          duration: dr,
+          easing: "linear",
+          complete: function () {
+            ob.trigger("marquee");
+          },
+          step: function () {
+            //check if scroller limits are reached
+            if (start_from == "right") {
+              if (parseInt(ob.position().left) < -parseInt(ob.width())) {
+                //we need to stop and restart animation
+                ob.stop();
+                ob.trigger("marquee");
+              }
+            } else {
+              if (
+                parseInt(ob.position().left) > parseInt(ob.parent().width())
+              ) {
+                ob.stop();
+                ob.trigger("marquee");
+              }
+            }
+          },
+        }
+      );
+    })
+    .trigger("marquee");
+  //pause scrolling animation on mouse over
+  scroller_obj.mouseover(function () {
+    $(this).stop();
+  });
+  //resume scrolling animation on mouse out
+  scroller_obj.mouseout(function () {
+    $(this).trigger("marquee", ["resume"]);
+  });
+}
+
+$(function () {
+  $(".text-animated").each(function (i, obj) {
+    if ($(this).find(".text-overflow").width() > $(this).width()) {
+      //settings to pass to function
+      var scroller = $(this).find(".text-overflow"); // element(s) to scroll
+      var scrolling_velocity = 95; // 1-99
+      var scrolling_from = "right"; // 'right' or 'left'
+      //call the function and start to scroll..
+      startScrolling(scroller, scrolling_velocity, scrolling_from);
+    }
+  });
+});
+
+/**
+ * END SCROLL TEXT
+ *
+ */
+
+$(".universal .btn-play-now").on("click", function () {
+  const language_value = $(
+    ".universal input[name='universal_radio']:checked"
+  ).val();
+  const allow_region = ["Malaysia", "Singapore"];
+  if (allow_region.includes(language_value)) {
+    window.location.href = "/login.html";
+  } else {
+    window.location.href = "/access-denied.html";
+  }
+});
